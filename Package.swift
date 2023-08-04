46769e099c9cfde3dff8db8242a89c6268beb1e9
David Kanenwisher <d.kanen@gmail.com>
2023-08-03 21:07:42 -0500

    Add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "lecs-swift",
+    platforms: [
+        .macOS(.v10_15),
+        .macCatalyst(.v16),
+        .iOS(.v16),
+        .tvOS(.v16),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
