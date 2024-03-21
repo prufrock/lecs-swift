c939f2f5d5a977224f2f021b3d7e0e2874cb40f9
David Kanenwisher <d.kanen@gmail.com>
2024-03-20 21:27:02 -0500

    Add documentation

--- Sources/lecs-swift/Archetypes/LECSComponentChart.swift
+++ Sources/lecs-swift/Archetypes/LECSComponentChart.swift
@@ -7,35 +7,50 @@
 
 import Foundation
 
+/// A Component that can be stored in a ComponentChart.
 protocol LECSCCComponent {
 
 }
 
+/// Stores Components on rows
 protocol LECSComponentChart {
+    /// Creates a row in the ComponentChart.
     func createRow() -> LECSCCRowId
 
+    /// Reads Component of the type from the given row.
     func readComponentFrom<T: LECSCCComponent>(row rowId: LECSRowId, type: T.Type)
 
+    /// Deletes the row.
     func delete(row rowId: LECSRowId)
 
+    /// Adds the LECSCCComponent to the row.
+    /// This makes the provided LECSRowId stale, so the caller needs to use the return LECSRowId.
     func addComponentTo(row rowId: LECSRowId, component: LECSCCComponent) -> LECSRowId
 
+    /// Removes the LECSCCComponent from the row.
+    /// This makes the provided LECSRowId stale, so the caller needs to use the return LECSRowId.
     func removeComponentFrom<T: LECSCCComponent>(row rowId: LECSRowId, type: T.Type) -> LECSRowId
 
-    func select(query: LECSQuery, read: (LECSRow, LECSCCColumns) -> Void)
+    /// Read all rows that have the Components in the query.
+    /// For now, need to use LECSCCColumns to read from LECSRow in the order of LECSQuery: LECSRow[LECSCColumns[0].col]
+    func select(query: LECSQuery, block: (LECSRow, LECSCCColumns) -> Void)
 
-    func update(query: LECSQuery, read: (LECSRow, LECSCCColumns) -> LECSRow)
+    /// Allows updates of the LECSRows selected by the LECSQuery.
+    /// The block should return the modified LECSRow to be stored in the ComponentChart.
+    func update(query: LECSQuery, block: (LECSRow, LECSCCColumns) -> LECSRow)
 }
 
-struct LECSCCQuery {
+/// Holds a list of the LECSComponents to match when querying the LECSComponentChart.
+struct LECSCCCQuery {
 
 }
 
-struct LECSCCArchetypeColumn {
-    
+// The column the LECSComponent is stored in a LECSCCCArchetype.
+struct LECSCCCArchetypeColumn {
+    let col: Int
 }
 
-typealias LECSCCColumns = [LECSCCArchetypeColumn]
+typealias LECSCCColumns = [LECSCCCArchetypeColumn]
 
 /// The ID of a row in an Archetype.
 struct LECSCCRowId {
